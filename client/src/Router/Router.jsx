ef2a1cbb72604411f1d69e80e0d0e83015cd9d4e
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2024-03-26 05:44:09 +0600

    register page sceleton added

--- client/src/Router/Router.jsx
+++ client/src/Router/Router.jsx
@@ -1,23 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import RootPage from "./RootPage";
-import Home from "../Pages/Home";
 import Dashboard from "../Pages/Dashboard";
 import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 export const Routes = createBrowserRouter([
   {
-    path: "/",
-    element: <RootPage />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
-  {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
   {
     path: "/dashboard",
